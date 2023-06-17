572a46b8c9d8f292eeb112f0f6131bdd83f1fc0e
James Fehr <Turtle-Head@users.noreply.github.com>
2023-06-16 17:10:35 -0700

    Add files via upload

--- Convert.cpp
+++ Convert.cpp
@@ -4,39 +4,51 @@
 #include <iostream>
 #include "BMICalculator.h"
 #include "SpeedCalculater.h"
-double speedCalc();
-double BMICalc();
+double speedCalc(bool m);
+double BMICalc(bool m);
 void menu();
 int main()
 {
     menu();
 }
 void metricChoice() {
-    std::cout << "\n Your Body mass index is " << BMICalc() << "\n";
-    std::cout << "\nSpeed = distance / time :" << speedCalc() << " kM/hr \n";
+    std::cout << "\n Your Body mass index is " << BMICalc(true) << "\n";
+    std::cout << "\nSpeed = distance / time :" << speedCalc(true) << " kM/hr \n";
 }
 void imperialChoice() {
-    std::cout << "\n Your Body mass index is " << BMICalc() << "\n";
-    std::cout << "\nSpeed = distance / time :" << speedCalc() << " Mph \n";
+    std::cout << "\n Your Body mass index is " << BMICalc(false) << "\n";
+    std::cout << "\nSpeed = distance / time :" << speedCalc(false) << " Mph \n";
 }
-double speedCalc() {
+double speedCalc(bool m) {
     double distance, time;
     std::cout << "\n Please enter distance: ";
     std::cin >> distance;
     std::cout << "\n Please enter time: ";
     std::cin >> time;
+    if (m == false) {
+        distance *= 1.60934;
+    }
     SpeedCalculater speed; 
     return speed.calculate(distance, time);
 }
 
-double BMICalc() {
+double BMICalc(bool m) {
     double weight, height;
     std::cout << "\nPlease enter your height: ";
     std::cin >> height;
     std::cout << "\nPlease enter your weight: ";
     std::cin >> weight;
+    if (m == false) {
+        height *= 0.3048;
+        weight *= 0.453592;
+    }
     BMICalculator BMI;
     return BMI.calculate(weight, height);
+}
+
+double speedCalc()
+{
+    return 0.0;
 }
 
 void menu() {
@@ -45,8 +57,8 @@ void menu() {
     int choice = 0;
 
     while (exit) {
-        std::cout << "\n--Converter--\n ";
-        std::cout << "- 0 - Exit Converter - \n";
+        std::cout << "\n--Calculator--\n ";
+        std::cout << "- 0 - Exit Calculator - \n";
         std::cout << " - 1 - Metric - \n";
         std::cout << " - 2 - Imperial - \n";
         std::cout << "- Please enter your preferred unit system: ";

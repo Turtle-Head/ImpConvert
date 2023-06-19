584442e38c6f712ffe88f62a036d2c93021dcc2f
James Fehr <Turtle-Head@users.noreply.github.com>
2023-06-19 13:15:51 -0700

    Add files via upload

--- Convert.cpp
+++ Convert.cpp
@@ -11,6 +11,15 @@
 #include "WeightConverter.h"
 #include "Calculator.h"
 #include "Converter.h"
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define CYAN "\033[36m"
+#define LIGHT_GREEN_TEXT "\033[92m"
+#define VIOLET_TEXT "\033[35m"
+
 
 void speedCalc(bool m);
 void BMICalc(bool m);
@@ -29,21 +38,21 @@ void speedCalc(bool m) {
     double distance, time, result;
     DistanceConverter actionObj{};
     SpeedCalculater display{};
-    std::cout << "\n Please enter distance: ";
+    std::cout << LIGHT_GREEN_TEXT << "\n Please enter distance: ";
     std::cin >> distance;
-    std::cout << "\n Please enter time: ";
+    std::cout << LIGHT_GREEN_TEXT << "\n Please enter time: ";
     std::cin >> time;
     if (m == false) { 
-        std::cout << " Distance in Miles: " << distance;
+        std::cout << LIGHT_GREEN_TEXT << " Distance in Miles: " << distance;
         // If Imperial selection was made we need to covert to maetric for the calculations
         distance = actionObj.toMetric(distance);
         // distance = dobj.toMetric(distance); // : used to convert imperial to metric
-        std::cout << " Distance in KM: " << distance << "\n";
+        std::cout << LIGHT_GREEN_TEXT << " Distance in KM: " << distance << "\n";
     }
     else if (m == true) {
-        std::cout << " Distance in KM: " << distance;
+        std::cout << LIGHT_GREEN_TEXT << " Distance in KM: " << distance;
           double distanceImperial = actionObj.toImperial(distance);
-        std::cout << " Distance in Miles: " << distanceImperial << "\n";
+        std::cout << LIGHT_GREEN_TEXT << " Distance in Miles: " << distanceImperial << "\n";
     }
    
     Calculate(&display, distance, time);
@@ -53,23 +62,23 @@ void BMICalc(bool m) {
     double weight, height, result;
     HeightConverter hobj{};   // Height Converter object : used to convert imperial to metric
     WeightConverter wobj{};   // Weight Converter object : used to convert imperial to metric
-    std::cout << "\nPlease enter your height: ";
+    std::cout << LIGHT_GREEN_TEXT << "\nPlease enter your height: ";
     std::cin >> height;
-    std::cout << "\nPlease enter your weight: ";
+    std::cout << LIGHT_GREEN_TEXT << "\nPlease enter your weight: ";
     std::cin >> weight;
     // If the request for imperial measurements is made if metric = false so we need to convert to metric for the calculatioins
     if (m == false) {
         std::cout << "\n Height in Feet: " << height;
         height = hobj.toMetric(height); // : used to convert imperial to metric
-        std::cout << " Height in Meters: " << height << "\n Weight in LBs: " << weight;
+        std::cout << LIGHT_GREEN_TEXT << " Height in Meters: " << height << "\n Weight in LBs: " << weight;
         weight = wobj.toMetric(weight); // : used to convert imperial to metric
-        std::cout << "\n Weight in KG: " << weight << "\n";
+        std::cout << LIGHT_GREEN_TEXT << "\n Weight in KG: " << weight << "\n";
     }
     else if (m == true) {
         double himp = hobj.toImperial(height);
-        std::cout << "\n Height M: " << height << " Ft: " << himp << " ";
+        std::cout << LIGHT_GREEN_TEXT << "\n Height M: " << height << " Ft: " << himp << " ";
         double wimp = wobj.toImperial(weight);
-        std::cout << " Weight KG: " << weight << " LBs: " << wimp << "\n";
+        std::cout << LIGHT_GREEN_TEXT << " Weight KG: " << weight << " LBs: " << wimp << "\n";
     }
     BMICalculator BMI{};
      Calculate(&BMI,weight,height); // Get BMI value
@@ -83,11 +92,12 @@ void menu() {
     
 
     while (exit) {
-        std::cout << "\n--Calculator--\n ";
-        std::cout << "- 0 - Exit Calculator - \n";
-        std::cout << " - 1 - Metric - \n";
-        std::cout << " - 2 - Imperial - \n";
-        std::cout << "- Please enter your preferred unit system: ";
+        std::cout << LIGHT_GREEN_TEXT << "\n--Calculator--\n ";
+        std::cout << LIGHT_GREEN_TEXT << "- 0 - " << YELLOW << "Exit Calculator - \n";
+        std::cout << LIGHT_GREEN_TEXT << " - 1 - " << RESET << "Metric - \n";
+        std::cout << LIGHT_GREEN_TEXT << " - 2 - " << RESET << "Imperial - \n";
+        std::cout << RESET << "- Please enter your preferred unit system: ";
+        std::cout << LIGHT_GREEN_TEXT;
         std::cin >> choice;
         switch (choice) {
         case 0: {
